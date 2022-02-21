af83b85ae17f8cdbb1c4be410787f0400de30bb9
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-21 10:02:46 +0500

    imports

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
 
 PORT = 8800;
 
