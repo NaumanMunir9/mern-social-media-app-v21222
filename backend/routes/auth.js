33e20352c9542f1a96cb96113f52ed7cf449ac15
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-21 14:35:00 +0500

    generate hashed password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,24 @@
 // libraries
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 // models
 const User = require("../models/user");
 
 // register user
 router.post("/register", async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
   try {
+    // generate hashed password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // generate new User
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // save user and return response
     const user = await newUser.save();
     console.log(`User ${JSON.stringify(user)} has been registered`);
     res.status(201).json(user);
