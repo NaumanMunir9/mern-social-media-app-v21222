3833f18a4051147af76c119363b12195b5836381
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-21 22:45:05 +0500

    delete a post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -30,6 +30,19 @@ router.put("/:id", async (req, res) => {
 });
 
 // delete a post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("The post has been deleted");
+    } else {
+      res.status(403).json("You are not authorized to delete this post");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // like a post
 
