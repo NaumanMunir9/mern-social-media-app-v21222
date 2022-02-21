2e836f8026d15f74b86476d2c3515b43e18d6d75
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-21 22:42:20 +0500

    update a post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -15,6 +15,19 @@ router.post("/", async (req, res) => {
 });
 
 // update a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("The post has been updated");
+    } else {
+      res.status(403).json("You are not authorized to update this post");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // delete a post
 
