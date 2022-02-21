73a826fd096a86697cc257b4bb030f3976dc0b65
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-21 22:17:23 +0500

    create a new post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,3 +1,27 @@
+// libraries
 const router = require("express").Router();
+// models
+const Post = require("../models/Post");
+
+// create a new post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// update a post
+
+// delete a post
+
+// like a post
+
+// get a post
+
+// get timeline posts
 
 module.exports = router;
