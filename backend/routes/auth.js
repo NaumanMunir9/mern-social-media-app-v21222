47e39cd1adc3cef621480f9574e8276706c08014
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-21 14:27:03 +0500

    register user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,24 @@
+// libraries
 const router = require("express").Router();
+// models
+const User = require("../models/user");
 
-router.get("/", (req, res) => {
-  res.send("Hey, This is the auth route");
+// register user
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const user = await newUser.save();
+    console.log(`User ${JSON.stringify(user)} has been registered`);
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(`Error Occurred while registering user: ${error}`);
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
