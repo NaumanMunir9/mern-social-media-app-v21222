570d6b11445553a4e7a40288582e91dd6ee936b6
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-21 10:10:21 +0500

    Middleware

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,11 @@ mongoose.connect(process.env.MONGO_URL, () => {
   console.log(`Connected to MongoDB at: ${process.env.MONGO_URL}`);
 });
 
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
 PORT = 8800;
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
