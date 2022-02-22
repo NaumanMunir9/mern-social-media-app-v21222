f142af1d930717188e7ca0bce72f26cef142aab3
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-22 11:43:36 +0500

    feed component

---
+++ frontend/components/Feed.jsx
@@ -0,0 +1,10 @@
+// styles
+import styles from "../styles/Feed.module.css";
+
+export default function Feed() {
+  return (
+    <div className={styles.feed}>
+      <h1>Feed</h1>
+    </div>
+  );
+}
