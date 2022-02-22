5980ee31628a82848958f1ed871bab82707472c9
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-22 11:43:57 +0500

    rightbar components

---
+++ frontend/components/Rightbar.jsx
@@ -0,0 +1,10 @@
+// styles
+import styles from "../styles/Rightbar.module.css";
+
+export default function Rightbar() {
+  return (
+    <div className={styles.rightbar}>
+      <h1>Rightbar</h1>
+    </div>
+  );
+}
