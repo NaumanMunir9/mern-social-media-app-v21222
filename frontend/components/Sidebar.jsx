26901848483229e9383ec9023f3b64d40d9c2a89
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-22 11:44:10 +0500

    sidebar components

---
+++ frontend/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+// styles
+import styles from "../styles/Sidebar.module.css";
+
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <h1>Sidebar</h1>
+    </div>
+  );
+}
