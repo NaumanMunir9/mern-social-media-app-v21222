44c4b670ae74cdd0a90b9c524ae3b7c748be82ec
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-22 12:03:20 +0500

    list items and icons

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -1,10 +1,69 @@
+// libraries
+import {
+  RssFeed,
+  Chat,
+  PlayCircleFilledOutlined,
+  Group,
+  Bookmark,
+  HelpOutline,
+  WorkOutline,
+  Event,
+  School,
+} from "@mui/icons-material";
 // styles
 import styles from "../styles/Sidebar.module.css";
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <h1>Sidebar</h1>
+      <div className={styles.sidebarWrapper}>
+        <ul className={styles.sidebarList}>
+          <li className={styles.sidebarListItem}>
+            <RssFeed className={styles.sidebarIcon} />
+            <span className={styles.sidebarListItemText}>Feed</span>
+          </li>
+
+          <li className={styles.sidebarListItem}>
+            <Chat className={styles.sidebarIcon} />
+            <span className={styles.sidebarListItemText}>Chats</span>
+          </li>
+
+          <li className={styles.sidebarListItem}>
+            <PlayCircleFilledOutlined className={styles.sidebarIcon} />
+            <span className={styles.sidebarListItemText}>Videos</span>
+          </li>
+
+          <li className={styles.sidebarListItem}>
+            <Group className={styles.sidebarIcon} />
+            <span className={styles.sidebarListItemText}>Groups</span>
+          </li>
+
+          <li className={styles.sidebarListItem}>
+            <Bookmark className={styles.sidebarIcon} />
+            <span className={styles.sidebarListItemText}>Bookmarks</span>
+          </li>
+
+          <li className={styles.sidebarListItem}>
+            <HelpOutline className={styles.sidebarIcon} />
+            <span className={styles.sidebarListItemText}>Questions</span>
+          </li>
+
+          <li className={styles.sidebarListItem}>
+            <WorkOutline className={styles.sidebarIcon} />
+            <span className={styles.sidebarListItemText}>Jobs</span>
+          </li>
+
+          <li className={styles.sidebarListItem}>
+            <Event className={styles.sidebarIcon} />
+            <span className={styles.sidebarListItemText}>Events</span>
+          </li>
+
+          <li className={styles.sidebarListItem}>
+            <School className={styles.sidebarIcon} />
+            <span className={styles.sidebarListItemText}>Courses</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
