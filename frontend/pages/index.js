02259973b4adc466eea42267a569cf3f1c46e840
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-22 10:31:50 +0500

    packages

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,11 @@
+// libraries
+import PersonIcon from "@mui/icons-material/Person";
+
 export default function HomePage() {
   return (
     <div>
       <h1>HomePage</h1>
+      <PersonIcon />
     </div>
   );
 }
