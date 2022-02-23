3b370173e213ef5b5213b7b7280e1024a45b22fc
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-23 22:08:37 +0500

    postTop

---
+++ frontend/components/Post.jsx
@@ -0,0 +1,33 @@
+// libraries
+import Image from "next/image";
+import { MoreVert } from "@mui/icons-material";
+// styles
+import styled from "../styles/Post.module.css";
+
+export default function Post() {
+  return (
+    <div className={styled.postContainer}>
+      <div className={styled.postWrapper}>
+        <div className={styled.postTop}>
+          <div className={styled.postTopLeft}>
+            <Image
+              className={styled.postProfileImg}
+              src="/person/1.jpeg"
+              width={40}
+              height={40}
+            />
+            <span className={styled.postUserName}>Nauman Munir</span>
+            <span className={styled.postDate}>5 mins ago</span>
+          </div>
+          <div className={styled.postTopRight}>
+            <MoreVert />
+          </div>
+        </div>
+
+        <div className={styled.postCenter}></div>
+
+        <div className={styled.postBottom}></div>
+      </div>
+    </div>
+  );
+}
