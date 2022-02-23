2461ff2abe599e55447fff4ac0a2953042f2d0b5
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-23 22:25:51 +0500

    Post Bottom

--- frontend/components/Post.jsx
+++ frontend/components/Post.jsx
@@ -2,31 +2,58 @@
 import Image from "next/image";
 import { MoreVert } from "@mui/icons-material";
 // styles
-import styled from "../styles/Post.module.css";
+import styles from "../styles/Post.module.css";
 
 export default function Post() {
   return (
-    <div className={styled.postContainer}>
-      <div className={styled.postWrapper}>
-        <div className={styled.postTop}>
-          <div className={styled.postTopLeft}>
+    <div className={styles.postContainer}>
+      <div className={styles.postWrapper}>
+        <div className={styles.postTop}>
+          <div className={styles.postTopLeft}>
             <Image
-              className={styled.postProfileImg}
+              className={styles.postProfileImg}
               src="/person/1.jpeg"
               width={32}
               height={32}
             />
-            <span className={styled.postUserName}>Nauman Munir</span>
-            <span className={styled.postDate}>5 mins ago</span>
+            <span className={styles.postUserName}>Nauman Munir</span>
+            <span className={styles.postDate}>5 mins ago</span>
           </div>
-          <div className={styled.postTopRight}>
+          <div className={styles.postTopRight}>
             <MoreVert />
           </div>
         </div>
 
-        <div className={styled.postCenter}></div>
+        <div className={styles.postCenter}>
+          <span className={styles.postText}>Hey! It's my first post</span>
+          <Image
+            className={styles.postImg}
+            src="/post/1.jpeg"
+            width={40}
+            height={40}
+          />
+        </div>
 
-        <div className={styled.postBottom}></div>
+        <div className={styles.postBottom}>
+          <div className={styles.postBottomLeft}>
+            <Image
+              src="/like.png"
+              width={32}
+              height={32}
+              className={styles.likeIcon}
+            />
+            <Image
+              src="/heart.png"
+              width={32}
+              height={32}
+              className={styles.likeIcon}
+            />
+            <span className={styles.postLikeCounter}>15 people like it</span>
+          </div>
+          <div className={styles.postBottomRight}>
+            <span className={styles.postCommentText}>6 comments</span>
+          </div>
+        </div>
       </div>
     </div>
   );
