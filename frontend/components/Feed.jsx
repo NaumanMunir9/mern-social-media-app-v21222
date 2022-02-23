13885a53f658103574d926797a26f34860e22072
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-23 19:55:23 +0500

    Feed

--- frontend/components/Feed.jsx
+++ frontend/components/Feed.jsx
@@ -1,10 +1,16 @@
+// components
+import Share from "./Share";
+import Post from "./Post";
 // styles
 import styles from "../styles/Feed.module.css";
 
 export default function Feed() {
   return (
     <div className={styles.feed}>
-      <h1>Feed</h1>
+      <div className={styles.feedWrapper}>
+        <Share />
+        <Post />
+      </div>
     </div>
   );
 }
