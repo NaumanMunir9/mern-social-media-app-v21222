0b44cec7ecc44ff399422ba893cea6d3aa9b5803
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-24 18:59:38 +0500

    6 post

--- frontend/components/Feed.jsx
+++ frontend/components/Feed.jsx
@@ -10,6 +10,11 @@ export default function Feed() {
       <div className={styles.feedWrapper}>
         <Share />
         <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   );
