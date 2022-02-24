9c17d3b9c5a143f4138bd8163764fa1a2f0dea67
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-24 19:31:32 +0500

    rightbarWrapper

--- frontend/components/Rightbar.jsx
+++ frontend/components/Rightbar.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 // pics
 import birthdayPic from "../public/gift.png";
+import adPic from "../public/ad.png";
 // styles
 import styles from "../styles/Rightbar.module.css";
 
@@ -10,12 +11,24 @@ export default function Rightbar() {
     <div className={styles.rightbarContainer}>
       <div className={styles.rightbarWrapper}>
         <div className={styles.birthdayContainer}>
-          <Image src={birthdayPic} width={40} height={40} />
-          <span className="birthdayText">
+          <Image
+            src={birthdayPic}
+            width={40}
+            height={40}
+            className={styles.birthdayImg}
+          />
+          <span className={styles.birthdayText}>
             <em>Muhammad</em> and <em>2 other friends</em> are celebrating their
             birthday!
           </span>
         </div>
+
+        <Image
+          src={adPic}
+          width={500}
+          height={350}
+          className={styles.rightbarAd}
+        />
       </div>
     </div>
   );
