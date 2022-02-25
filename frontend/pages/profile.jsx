95d85b5d808db92ea23c6a3ae5863fd9e2f1bc8e
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-25 22:28:53 +0500

    styling of page

--- frontend/pages/profile.jsx
+++ frontend/pages/profile.jsx
@@ -14,25 +14,28 @@ export default function ProfilePage() {
         <div className={styles.profileRight}>
           <div className={styles.profileRightTop}>
             <div className={styles.profileCover}>
-              <div className={styles.profileCoverImg}>
+              <div className={styles.profileCoverImgDiv}>
                 <Image
                   src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  width={640}
-                  height={426}
+                  layout="fill"
+                  className={styles.profileCoverImg}
                 />
               </div>
-              <div className={styles.profileUserImg}>
+              <div className={styles.profileUserImgDiv}>
                 <Image
                   src="https://avatars.githubusercontent.com/u/41162751?v=4"
-                  width={128}
-                  height={128}
+                  width={150}
+                  height={150}
+                  className={styles.profileUserImg}
                 />
               </div>
             </div>
 
             <div className={styles.profileInfo}>
               <h4 className={styles.profileInfoName}>MNM</h4>
-              <span className={styles.profileDesc}>This is User Desc</span>
+              <span className={styles.profileDesc}>
+                Full Stack Software Developer and Blockchain Engineer
+              </span>
             </div>
           </div>
 
