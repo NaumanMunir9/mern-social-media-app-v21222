446007b9a4ac46a37f997ac2302e2eec861f9e42
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-25 19:28:53 +0500

    export components

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -3,3 +3,7 @@ export { default as Seo } from "./Seo";
 export { default as Feed } from "./Feed";
 export { default as Rightbar } from "./Rightbar";
 export { default as Sidebar } from "./Sidebar";
+export { default as Post } from "./Post";
+export { default as Share } from "./Share";
+export { default as Online } from "./Online";
+export { default as CloseFriends } from "./CloseFriends";
