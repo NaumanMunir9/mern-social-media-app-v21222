83054614d8296d2b09ecdc4e551b75d22cea30a6
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-25 19:39:56 +0500

    ProfilePage

--- frontend/pages/profile.jsx
+++ frontend/pages/profile.jsx
@@ -1,10 +1,47 @@
+// components
+import Image from "next/image";
+import { Seo, Topbar, Sidebar, Feed, Rightbar } from "../components";
 // styles
 import styles from "../styles/Profile.module.css";
 
 export default function ProfilePage() {
   return (
-    <div>
-      <h1>profile</h1>
-    </div>
+    <>
+      <Seo />
+      <Topbar />
+      <div className={styles.profileContainer}>
+        <Sidebar />
+        <div className={styles.profileRight}>
+          <div className={styles.profileRightTop}>
+            <div className={styles.profileCover}>
+              <div className={styles.profileCoverImg}>
+                <Image
+                  src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                  width={640}
+                  height={426}
+                />
+              </div>
+              <div className={styles.profileUserImg}>
+                <Image
+                  src="https://avatars.githubusercontent.com/u/41162751?v=4"
+                  width={128}
+                  height={128}
+                />
+              </div>
+            </div>
+
+            <div className={styles.profileInfo}>
+              <h4 className={styles.profileInfoName}>MNM</h4>
+              <span className={styles.profileDesc}>This is User Desc</span>
+            </div>
+          </div>
+
+          <div className={styles.profileRightBottom}>
+            <Feed />
+            <Rightbar />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
