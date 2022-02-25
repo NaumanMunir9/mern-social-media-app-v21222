5adfa514aa3b4e4ecee184f3b5f5136bb5b28614
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-25 22:42:56 +0500

    ProfileRightbar

--- frontend/components/Rightbar.jsx
+++ frontend/components/Rightbar.jsx
@@ -6,10 +6,10 @@ import { Users } from "../dummyData";
 import styles from "../styles/Rightbar.module.css";
 import Online from "./Online";
 
-export default function Rightbar() {
-  return (
-    <div className={styles.rightbarContainer}>
-      <div className={styles.rightbarWrapper}>
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return (
+      <>
         <div className={styles.birthdayContainer}>
           <Image
             src="https://images.unsplash.com/photo-1578922864601-79dcc7cbcea9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
@@ -39,6 +39,102 @@ export default function Rightbar() {
             ))}
           </ul>
         </div>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className={styles.rightbarTitle}>This is Profile Page Rightbar</h4>
+
+        <div className={styles.rightbarInfo}>
+          <div className={styles.rightbarInfoItem}>
+            <span className={styles.rightbarInfoKey}>City:</span>
+            <span className={styles.rightbarInfoValue}>Karachi</span>
+          </div>
+
+          <div className={styles.rightbarInfoItem}>
+            <span className={styles.rightbarInfoKey}>From:</span>
+            <span className={styles.rightbarInfoValue}>Pakistan</span>
+          </div>
+
+          <div className={styles.rightbarInfoItem}>
+            <span className={styles.rightbarInfoKey}>Relationship:</span>
+            <span className={styles.rightbarInfoValue}>Single</span>
+          </div>
+        </div>
+
+        <h4 className={styles.rightbarTitle}>Users Friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <Image
+              src="https://avatars.githubusercontent.com/u/41162751?v=4"
+              width={40}
+              height={40}
+              className={styles.rightbarFollowingImg}
+            />
+            <span className={styles.rightbarFollowingName}>Muhammad</span>
+          </div>
+
+          <div className="rightbarFollowing">
+            <Image
+              src="https://avatars.githubusercontent.com/u/41162751?v=4"
+              width={40}
+              height={40}
+              className={styles.rightbarFollowingImg}
+            />
+            <span className={styles.rightbarFollowingName}>Muhammad</span>
+          </div>
+
+          <div className="rightbarFollowing">
+            <Image
+              src="https://avatars.githubusercontent.com/u/41162751?v=4"
+              width={40}
+              height={40}
+              className={styles.rightbarFollowingImg}
+            />
+            <span className={styles.rightbarFollowingName}>Muhammad</span>
+          </div>
+
+          <div className="rightbarFollowing">
+            <Image
+              src="https://avatars.githubusercontent.com/u/41162751?v=4"
+              width={40}
+              height={40}
+              className={styles.rightbarFollowingImg}
+            />
+            <span className={styles.rightbarFollowingName}>Muhammad</span>
+          </div>
+
+          <div className="rightbarFollowing">
+            <Image
+              src="https://avatars.githubusercontent.com/u/41162751?v=4"
+              width={40}
+              height={40}
+              className={styles.rightbarFollowingImg}
+            />
+            <span className={styles.rightbarFollowingName}>Muhammad</span>
+          </div>
+
+          <div className="rightbarFollowing">
+            <Image
+              src="https://avatars.githubusercontent.com/u/41162751?v=4"
+              width={40}
+              height={40}
+              className={styles.rightbarFollowingImg}
+            />
+            <span className={styles.rightbarFollowingName}>Muhammad</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className={styles.rightbarContainer}>
+      <div className={styles.rightbarWrapper}>
+        <ProfileRightbar />
       </div>
     </div>
   );
