50865534d9abbef1d2ebdd76a2fc05135841ae65
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-25 10:57:00 +0500

    dummyData

---
+++ frontend/dummyData.js
@@ -0,0 +1,110 @@
+export const Users = [
+  {
+    id: 1,
+    username: "MM",
+    profileImg: "https://avatars.githubusercontent.com/u/41162751?v=4",
+  },
+  {
+    id: 2,
+    username: "SM",
+    profileImg: "https://avatars.githubusercontent.com/u/41162751?v=4",
+  },
+  {
+    id: 3,
+    username: "HM",
+    profileImg: "https://avatars.githubusercontent.com/u/41162751?v=4",
+  },
+  {
+    id: 4,
+    username: "MH",
+    profileImg: "https://avatars.githubusercontent.com/u/41162751?v=4",
+  },
+  {
+    id: 5,
+    username: "MUM",
+    profileImg: "https://avatars.githubusercontent.com/u/41162751?v=4",
+  },
+  {
+    id: 6,
+    username: "MNM",
+    profileImg: "https://avatars.githubusercontent.com/u/41162751?v=4",
+  },
+  {
+    id: 7,
+    username: "AMJ",
+    profileImg: "https://avatars.githubusercontent.com/u/41162751?v=4",
+  },
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel tincidunt congue, nisl nunc aliquet nunc, eget euismod nisl nunc euismod nisl nunc.",
+    photo:
+      "https://images.unsplash.com/photo-1452421822248-d4c2b47f0c81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    date: "1 mins ago",
+    userId: 1,
+    like: 12,
+    comment: 6,
+  },
+  {
+    id: 2,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel tincidunt congue, nisl nunc aliquet nunc, eget euismod nisl nunc euismod nisl nunc.",
+    photo:
+      "https://images.unsplash.com/photo-1542359649-31e03cd4d909?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    date: "2 mins ago",
+    userId: 2,
+    like: 14,
+    comment: 7,
+  },
+  {
+    id: 3,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel tincidunt congue, nisl nunc aliquet nunc, eget euismod nisl nunc euismod nisl nunc.",
+    photo:
+      "https://images.unsplash.com/photo-1512036666432-2181c1f26420?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80",
+    date: "3 mins ago",
+    userId: 3,
+    like: 16,
+    comment: 8,
+  },
+  {
+    id: 4,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel tincidunt congue, nisl nunc aliquet nunc, eget euismod nisl nunc euismod nisl nunc.",
+    photo:
+      "https://images.unsplash.com/photo-1521336575822-6da63fb45455?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    date: "4 mins ago",
+    userId: 4,
+    like: 10,
+    comment: 5,
+  },
+  {
+    id: 5,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel tincidunt congue, nisl nunc aliquet nunc, eget euismod nisl nunc euismod nisl nunc.",
+    photo:
+      "https://images.unsplash.com/photo-1618083707368-b3823daa2726?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    date: "5 mins ago",
+    userId: 5,
+    like: 10,
+    comment: 5,
+  },
+  {
+    id: 6,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel tincidunt congue, nisl nunc aliquet nunc, eget euismod nisl nunc euismod nisl nunc.",
+    photo:
+      "https://images.unsplash.com/photo-1542235222-30e843cb43a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    date: "6 mins ago",
+    userId: 6,
+    like: 18,
+    comment: 9,
+  },
+  {
+    id: 7,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel tincidunt congue, nisl nunc aliquet nunc, eget euismod nisl nunc euismod nisl nunc.",
+    photo:
+      "https://images.unsplash.com/photo-1498889444388-e67ea62c464b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80",
+    date: "7 mins ago",
+    userId: 7,
+    like: 20,
+    comment: 10,
+  },
+];
