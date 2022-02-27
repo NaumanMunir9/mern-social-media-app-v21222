48cd0b05d5496c649ed5e6f615c98fba8c8685bc
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-27 21:35:44 +0500

    loginLeft

---
+++ frontend/pages/login.jsx
@@ -0,0 +1,18 @@
+// styles
+import styles from "../styles/Login.module.css";
+
+export default function login() {
+  return (
+    <div className={styles.loginContainer}>
+      <div className={styles.loginWrapper}>
+        <div className={styles.loginLeft}>
+          <h3 className={styles.loginLogo}>MNM</h3>
+          <span className={styles.loginDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </span>
+        </div>
+        <div className={styles.loginRight}></div>
+      </div>
+    </div>
+  );
+}
