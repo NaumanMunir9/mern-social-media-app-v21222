f1792a09f8dc7c768de0ac3e3ee752035b792e47
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-27 21:43:07 +0500

    Seo

--- frontend/pages/login.jsx
+++ frontend/pages/login.jsx
@@ -1,20 +1,26 @@
+// components
+import { Seo } from "../components";
 // styles
 import styles from "../styles/Login.module.css";
 
 export default function login() {
   return (
-    <div className={styles.loginContainer}>
-      <div className={styles.loginWrapper}>
-        <div className={styles.loginLeft}>
-          <h3 className={styles.loginLogo}>MNM</h3>
-          <span className={styles.loginDesc}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </span>
-        </div>
-        <div className={styles.loginRight}>
-          <div className={styles.loginBox}>Login box</div>
+    <>
+      <Seo />
+      <div className={styles.loginContainer}>
+        <div className={styles.loginWrapper}>
+          <div className={styles.loginLeft}>
+            <h3 className={styles.loginLogo}>MNM</h3>
+            <span className={styles.loginDesc}>
+              MNM social helps you connect and share with the people in your
+              life.
+            </span>
+          </div>
+          <div className={styles.loginRight}>
+            <div className={styles.loginBox}>Login box</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
